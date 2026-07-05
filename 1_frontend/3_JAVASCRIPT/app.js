1bc99be5c843ceecf09785fa4d3c2b53b6561d2c
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-05 07:50:26 +0530

    class work of js chap 1

---
+++ 1_frontend/3_JAVASCRIPT/app.js
@@ -0,0 +1,19 @@
+console.log("Hell World" );
+console.log("worst people ");
+let a= 10;
+let b = 3 ;
+console.log(`Sum is : ${a + b}`);
+
+let pencilPrice = 10;
+let penPrice = 5;
+console.log(`The total price is : ${pencilPrice + penPrice} Ruppes`);
+
+
+console.log(a**b);
+// conditional statements
+ console.log("before my if statement ");
+ let age = 23;
+ if(age>=17){
+    console.log("you can vote");
+ }
+ console.log("after my if statement ");
