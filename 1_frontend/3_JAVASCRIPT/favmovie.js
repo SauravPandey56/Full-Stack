d6f0425e7af8370102ca8c5c0f88c5f55a96cee8
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-07 01:28:47 +0530

    change in if condition which is inside the while loop

--- 1_frontend/3_JAVASCRIPT/favmovie.js
+++ 1_frontend/3_JAVASCRIPT/favmovie.js
@@ -2,9 +2,9 @@ const favMovie = "avenger";
 
 let guess = prompt ("guess my favourite movie ");
 
- while((guess != favMovie) ){
-    if(guess != "quit"){
-        console.log("you quit ")
+ while(guess != favMovie) {
+    if(guess == "quit"){
+        console.log("you quit")
         break;
     }
      guess= prompt("wrong guess,Please try again !")
