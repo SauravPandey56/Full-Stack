68560c3181294713b9c4bab61c440cd047f71fce
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-07 01:24:39 +0530

    changing in while loop

--- 1_frontend/3_JAVASCRIPT/favmovie.js
+++ 1_frontend/3_JAVASCRIPT/favmovie.js
@@ -2,12 +2,13 @@ const favMovie = "avenger";
 
 let guess = prompt ("guess my favourite movie ");
 
- while((guess != favMovie) &&(guess != "quit")){
+ while((guess != favMovie) ){
+    if(guess != "quit"){
+        console.log("you quit ")
+    }
      guess= prompt("wrong guess,Please try again !")
  }
- 
+
  if (guess == favMovie){
     console.log("CONGRATS !! ");
- }else{
-    console.log("you quit");
  }
