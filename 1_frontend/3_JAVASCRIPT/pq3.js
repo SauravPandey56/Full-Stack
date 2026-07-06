d9361036eccd3dadd4aa7503404898b43d23793d
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-06 23:38:19 +0530

    1 ,2,3 PQ OF CLASS WORK

---
+++ 1_frontend/3_JAVASCRIPT/pq3.js
@@ -0,0 +1,31 @@
+let msg = "   help  !  "
+let newmsg = msg.trim().toUpperCase();
+console.log(newmsg);
+
+let name = "apnaCollege";
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("apna","our"));
+let newName = name.slice(4).replaceAll('l','t');
+console.log(newName);
+
+// 2nd pq
+
+let months = ["january","july","march", "august"];
+console.log(months);
+months.shift();
+months.shift();
+months.unshift("june");
+months.unshift("july");
+console.log(months);
+
+//3rd pq 
+let month = ["january","july","march", "august"];
+month.splice(0,2,"july","june");
+console.log("month");
+
+let lngs = ["c","c++","html","javascript","python","java","c#"]
+lngs.push("sql");
+let newLngs = lngs.reverse().indexOf("javascript");
+console.log(lngs);
+console.log(newLngs);
