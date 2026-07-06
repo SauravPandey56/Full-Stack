d29d16563af0576ce8e6325eb23212aa78c2f6e3
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-07 01:25:33 +0530

    break using in while loop

--- 1_frontend/3_JAVASCRIPT/favmovie.js
+++ 1_frontend/3_JAVASCRIPT/favmovie.js
@@ -5,6 +5,7 @@ let guess = prompt ("guess my favourite movie ");
  while((guess != favMovie) ){
     if(guess != "quit"){
         console.log("you quit ")
+        break;
     }
      guess= prompt("wrong guess,Please try again !")
  }
