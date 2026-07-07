d3400a25eec7690ab706e964a5bfd3ba101cae0f
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-07 19:52:33 +0530

    class work of chapter 5

--- 1_frontend/3_JAVASCRIPT/chap5.js
+++ 1_frontend/3_JAVASCRIPT/chap5.js
@@ -1,9 +1,89 @@
-//creating a post by OBJECT LITERALS
-const post ={
-    userName:"@sauravpandey",
-    content:"this is my #fristPost",
-    likes:130,
-    reposts:4,
-    tags:["@apnacollege","@delta"]
-};
-console.log(post);
+// //creating a post by OBJECT LITERALS
+// const post ={
+//     userName:"@sauravpandey",
+//     content:"this is my #fristPost",
+//     likes:130,
+//     reposts:4,
+//     tags:["@apnacollege","@delta"]
+// };
+// console.log(post);
+// let prop = "reposts";   
+
+// //number get automatically converted in string
+ 
+// const obj = {
+//     1:"a",
+//     2:"b",
+//     true:"c",
+//     null:"d",
+//     undefined:"e"
+// };
+
+// // Add/update value 
+// const student = {
+//     name:"saurav",
+//     age:22,
+//     marks : 83.3,
+//     city :"patna"
+// };
+// console.log(student);
+
+// student.city = "Barh";
+// student.gender="male";
+// student.marks=[99,45,54];
+// console.log(student);
+// delete student.gender;
+
+// // objects of object
+// const classInfo={
+//     aman:{
+//         grade:"A+",
+//         city:"Delhi"
+//     },
+//     shradha:{
+//         grade:"A",
+//         city:"pune"
+//     },
+//     karan:{
+//         grade:"o",
+//         city:"Mumbai"
+//     }
+// };
+// classInfo.aman;
+// classInfo.shradha;
+// classInfo.shradha.city;
+// classInfo.shradha.city="patna";
+// console.log("classInfo");
+
+// // Array of objects
+// const classInfo =[
+//     {
+//         name:"aman",
+//         grade:"A+",
+//         city:"delhi"
+//     },
+//     {
+//         name:"shradha",
+//         grade:"A",
+//         city:"pune"
+//     },
+//     {
+//         name:"karan",
+//         grade:"o",
+//         city:"Mumbai"
+//     }
+// ]
+// classInfo[0];
+// classInfo[1].name;
+// classInfo[1].city="gurgaon";
+
+// Random Integer 
+let random=Math.floor(Math.random()*10)+1;
+console.log(random);
+
+let random1=Math.floor(Math.random()*100)+1;
+console.log(random1);
+let random2=Math.floor(Math.random()*5)+1;
+console.log(random2);
+let random3=Math.floor(Math.random()*10)+21;
+console.log(random3);
