241f110d9982df7b479ea96d1d5aa836230b425d
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-07 16:51:57 +0530

    creating a post by using object literals

---
+++ 1_frontend/3_JAVASCRIPT/chap5.js
@@ -0,0 +1,9 @@
+//creating a post by OBJECT LITERALS
+const post ={
+    userName:"@sauravpandey",
+    content:"this is my #fristPost",
+    likes:130,
+    reposts:4,
+    tags:["@apnacollege","@delta"]
+};
+console.log(post);
