c744afeab4c929880517227917956105c6031874
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-07 21:53:25 +0530

    corner case of this game add in else if condition

--- 1_frontend/3_JAVASCRIPT/guessingGame.js
+++ 1_frontend/3_JAVASCRIPT/guessingGame.js
@@ -13,7 +13,9 @@ while(true){
     if (guess == random){
         console.log("you are right ! congrats !! random number was :",random);
         break;
+    }else if(guess < random){
+        guess = prompt ("hint : your guess was too small . please try again ");
     }else{
-        guess = prompt ("your guess was wrong . please try again ");
+        guess = prompt ("hint : your guess was too large  . please try again ");
     }
 }
