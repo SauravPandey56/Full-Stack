0b40b3bad827e4565af3e3ec22d241cafbb79aee
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-08 23:47:52 +0530

    1 q of Assignment

---
+++ 1_frontend/3_JAVASCRIPT/assignment3.js
@@ -0,0 +1,5 @@
+//WriteaJavaScriptprogramtogetthefirstnelementsofanarray.[ncanbeanypositivenumber].Forexample:forarray[7,9,0,-2]andn=3Print,[7,9,0]
+letarr=[7,9,0,-2];
+letn=3;
+letans=arr.slice(0,n);
+console.log(ans);
