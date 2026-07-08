3b23b1537671c41d1b2b2dcc45d15c49fbdbbac7
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-08 23:53:06 +0530

    q 2 of assignment

--- 1_frontend/3_JAVASCRIPT/assignment3.js
+++ 1_frontend/3_JAVASCRIPT/assignment3.js
@@ -1,5 +1,13 @@
-//WriteaJavaScriptprogramtogetthefirstnelementsofanarray.[ncanbeanypositivenumber].Forexample:forarray[7,9,0,-2]andn=3Print,[7,9,0]
-letarr=[7,9,0,-2];
-letn=3;
-letans=arr.slice(0,n);
+//1.Write a JavaScript program to get the first n elements of an array.
+// [n can be any positive number].For example: for array[7,9,0,-2]and n=3 Print,[7,9,0]
+let arr=[7,9,0,-2];
+let n=3;
+let ans = arr.slice(0,n);
+console.log(ans);
+
+2.//Write a JavaScript program to get the last n elements of an array. 
+//[n can be any positive number].For example:fo rarray[7,9,0,-2]and n=3 Print,[9,0,-2]
+let arr=[7,9,0,-2]; 
+let n=3;
+let ans =arr.slice(arr.length-n); 
 console.log(ans);
