702a256ae3a3ba68a2f50884cc9efdaaa0f9b2bf
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-13 01:53:03 +0530

    question 2

--- 1_frontend/3_JAVASCRIPT/assignment5.js
+++ 1_frontend/3_JAVASCRIPT/assignment5.js
@@ -1,3 +1,14 @@
 //Qs1.Create a program that generate a random number representing a dice roll.
 let diceRoll = Math.floor(Math.random()*6)+1;
 console.log(diceRoll);
+
+// Qs2.Create an object representing a car that stores the following properties for the
+//car:name,model,color
+//Print the car’s name.
+
+const car = {
+    name:"Mercdes",
+    model:"maybach",
+    color:"black"
+};
+console.log(car);
