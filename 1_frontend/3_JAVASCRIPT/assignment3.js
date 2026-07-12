1e1ea952664ee03808ae3459b83129ddf80acdcb
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-12 23:32:32 +0530

    question 3

--- 1_frontend/3_JAVASCRIPT/assignment3.js
+++ 1_frontend/3_JAVASCRIPT/assignment3.js
@@ -1,13 +1,22 @@
 //1.Write a JavaScript program to get the first n elements of an array.
 // [n can be any positive number].For example: for array[7,9,0,-2]and n=3 Print,[7,9,0]
-let arr=[7,9,0,-2];
-let n=3;
-let ans = arr.slice(0,n);
-console.log(ans);
+// let arr=[7,9,0,-2];
+// let n=3;
+// let ans = arr.slice(0,n);
+// console.log(ans);
 
 2.//Write a JavaScript program to get the last n elements of an array. 
 //[n can be any positive number].For example:fo rarray[7,9,0,-2]and n=3 Print,[9,0,-2]
-let arr=[7,9,0,-2]; 
-let n=3;
-let ans =arr.slice(arr.length-n); 
-console.log(ans);
+// let arr=[7,9,0,-2]; 
+// let n=3;
+// let ans =arr.slice(arr.length-n); 
+// console.log(ans);
+
+// Qs3.Write a JavaScript program to check whether a string is blank or not.
+// let ans = prompt("enter a string ");
+// if (ans.length == 0){
+//     console.log("string is empty");
+
+// }else {
+//     console.log("string is not blank");
+// }
