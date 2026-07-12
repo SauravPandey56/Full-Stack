4d7b51b431a3316584584189d76ad36db74e7df5
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-12 23:57:02 +0530

    assignment 3 is completed

--- 1_frontend/3_JAVASCRIPT/assignment3.js
+++ 1_frontend/3_JAVASCRIPT/assignment3.js
@@ -1,18 +1,18 @@
-//1.Write a JavaScript program to get the first n elements of an array.
+1.Write a JavaScript program to get the first n elements of an array.
 // [n can be any positive number].For example: for array[7,9,0,-2]and n=3 Print,[7,9,0]
 // let arr=[7,9,0,-2];
 // let n=3;
 // let ans = arr.slice(0,n);
 // console.log(ans);
 
-2.//Write a JavaScript program to get the last n elements of an array. 
+2.Write a JavaScript program to get the last n elements of an array. 
 //[n can be any positive number].For example:fo rarray[7,9,0,-2]and n=3 Print,[9,0,-2]
 // let arr=[7,9,0,-2]; 
 // let n=3;
 // let ans =arr.slice(arr.length-n); 
 // console.log(ans);
 
-// Qs3.Write a JavaScript program to check whether a string is blank or not.
+ Qs3.Write a JavaScript program to check whether a string is blank or not.
 // let ans = prompt("enter a string ");
 // if (ans.length == 0){
 //     console.log("string is empty");
@@ -21,7 +21,7 @@
 //     console.log("string is not blank");
 // }
 
-//  Qs4.Write a JavaScript program to test whether the character at the given (character) index is lower case.
+  Qs4.Write a JavaScript program to test whether the character at the given (character) index is lower case.
 // let str = prompt("enter the string");
 // let idx = prompt("enter the index for checking charcter case");
 // if(str[idx]== str[idx].toLowerCase()){
@@ -30,18 +30,19 @@
 //     console.log("not in lower case");
 // }
 
-//Qs5.Write a JavaScript program to strip leading andt railing spaces from a string.
+Qs5.Write a JavaScript program to strip leading andt railing spaces from a string.
 // let str = prompt("enter the string ");
 // console.log(`original string : ${str}`);
 // console.log(`without extra space : ${str.trim()}`);
 
 
-// Qs6.Write a JavaScript program to check if an element exists in an array or not.
-let arr =["helloo " ,'a',65,32,-565,535];
-let item = 64;
- if (arr.includes(item) != false){
-    console.log('elements exists');
+Qs6.Write a JavaScript program to check if an element exists in an array or not.
 
- }else{
-    console.log("element not exists");
- }
+// let arr =["helloo " ,'a',65,32,-565,535];
+// let item = 64;
+//  if (arr.includes(item) != false){
+//     console.log('elements exists');
+
+//  }else{
+//     console.log("element not exists");
+//  }
