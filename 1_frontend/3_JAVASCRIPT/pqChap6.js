d71eb9f86be26c307f57b6b34091aad5906772f6
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-17 14:57:15 +0530

    function in js ,

---
+++ 1_frontend/3_JAVASCRIPT/pqChap6.js
@@ -0,0 +1,22 @@
+// function in js 
+function hello(){
+    console.log("hello");
+}
+hello();
+
+// 1PQ
+//create a function that print a poem
+
+function printPoem(){
+    console.log("twinkwl twinkwl little star");
+    console.log("how i wonder what you are");
+}
+printPoem();
+
+//2PQ Create a function to roll  a dice and always display the value of the dice (1 to 6)
+
+function diceRoll(){
+    let rand = Math.floor(Math.random()*6)+1;
+    console.log(rand);
+}
+diceRoll();
