0b5a011e0627af9f8c6cd6f70d2f24c8049c1890
Saurav Pandey <pandeysaurav108@gmail.com>
2026-07-17 22:02:57 +0530

    this with Arrow functions

--- 1_frontend/3_JAVASCRIPT/pqchap7.js
+++ 1_frontend/3_JAVASCRIPT/pqchap7.js
@@ -1,70 +1,97 @@
-// THIS KEYWORD 
-const student = {
-    name : "saurav",
-    age : 21,
-    eng : 95,
-    math:93 ,
-    phy : 97,
-    getAvg(){
-        console.log(this);
-        let avg = (this.eng + this.math+this.phy)/3;
-        console.log(`${this.name} got avg marks = ${avg}`);
-    }
-}
+// // THIS KEYWORD 
+// const student = {
+//     name : "saurav",
+//     age : 21,
+//     eng : 95,
+//     math:93 ,
+//     phy : 97,
+//     getAvg(){
+//         console.log(this);
+//         let avg = (this.eng + this.math+this.phy)/3;
+//         console.log(`${this.name} got avg marks = ${avg}`);
+//     }
+// }
 
-function getAvg(){
-    console.log(this);
-}
+// function getAvg(){
+//     console.log(this);
+// }
 
-// try and catch
+// // try and catch
 
-console.log("hello");
-try{
-    console.log(a);
-}catch(err){
-    console.log("caught an error ...");
-    console.log(err);
-}
-console.log("hello2");
+// console.log("hello");
+// try{
+//     console.log(a);
+// }catch(err){
+//     console.log("caught an error ...");
+//     console.log(err);
+// }
+// console.log("hello2");
 
-// Arrow Function
-const sum = (a,b) =>{
-    console.log(a+b);
-};
-sum(4,5);
+// // Arrow Function
+// const sum = (a,b) =>{
+//     console.log(a+b);
+// };
+// sum(4,5);
 
-const square = n =>{
-    console.log(n*n);
-};
+// const square = n =>{
+//     console.log(n*n);
+// };
 
-square(4);
+// square(4);
 
-//  IMPLICIT RETURN IN ARROW Function 
+// //  IMPLICIT RETURN IN ARROW Function 
 
-const mul = (a,b)=>(
-    a*b
-);
-console.log(mul(3,4));
+// const mul = (a,b)=>(
+//     a*b
+// );
+// console.log(mul(3,4));
 
-// SET TIMEOUT
+// // SET TIMEOUT
  
-console.log("hi there !");
-setTimeout(() => {
-    console.log("apna college");
-}, 4000);
-console.log ("welcome  to ");
+// console.log("hi there !");
+// setTimeout(() => {
+//     console.log("apna college");
+// }, 4000);
+// console.log ("welcome  to ");
 
-// set interval 
+// // set interval 
 
-setInterval(() => {
-    console.log("saurav ");
-}, 2000);
+// setInterval(() => {
+//     console.log("saurav ");
+// }, 2000);
 
-// problem in setINterval fixed 
-let id =setInterval(() => {
-             console.log("hello ");
-        }, 2000);
+// // problem in setINterval fixed 
+// let id =setInterval(() => {
+//              console.log("hello ");
+//         }, 2000);
 
-console.log(`id is : ${id}`);
+// console.log(`id is : ${id}`);
 
-clearInterval(id);        
+// clearInterval(id);        
+
+// this with Arrow functions
+
+const studnt = {
+    name:"aman",
+    marks:95,
+    prop : this , // global scope
+    getname : function (){
+        console.log(this);
+        return this .name ; 
+    },
+    getmarks:()=>{
+        console.log(this); // parent's scope-> window object
+        return this.marks
+    },
+    getInfo1:function(){
+        setTimeout(()=>{
+            console.log(this); // student 
+        },2000);
+    },
+    getInfo2:function(){
+        setTimeout(function (){
+            console.log(this); // window  
+        },2000);
+    },
+};
+studnt. getInfo1();
