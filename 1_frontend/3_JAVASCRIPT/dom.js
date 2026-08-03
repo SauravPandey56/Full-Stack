190371eaf2744f1d18a93717e2b101b8611802d0
Saurav Pandey <pandeysaurav108@gmail.com>
2026-08-03 19:13:41 +0530

    DOM getElementsByClassName

---
+++ 1_frontend/3_JAVASCRIPT/dom.js
@@ -0,0 +1,18 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i = 0 ; i<smallImages.length ; i++){
+//     console.dir(smallImages[i]);
+// }
+
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i = 0 ; i<smallImages.length ; i++){
+//     console.dir(smallImages[i].src);
+// }
+
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i = 0 ; i<smallImages.length ; i++){
+//     smallImages[i].src = "assets/spiderman_img.pmg";
+//     console.log (`value of image no. ${i}is changed.`);
+// }
