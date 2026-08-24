52b0698fb65298f5de0df9391912ae97d6aa10a4
Saurav Pandey <pandeysaurav108@gmail.com>
2026-08-24 18:06:04 +0530

    event listners for elements

--- 1_frontend/3_JAVASCRIPT/chap10.js
+++ 1_frontend/3_JAVASCRIPT/chap10.js
@@ -18,20 +18,31 @@
 // }
 
 // event listner 
-let btns = document.querySelectorAll("button");
- for(btn of btns){
-    btn.addEventListener("click",sayHello);
-    btn.addEventListener("click",sayName);
-    btn.addEventListener("dblclick",function(){
-        console.log("dek dek kaise dekh raha hai ...");
+// let btns = document.querySelectorAll("button");
+//  for(btn of btns){
+//     btn.addEventListener("click",sayHello);
+//     btn.addEventListener("click",sayName);
+//     btn.addEventListener("dblclick",function(){
+//         console.log("dek dek kaise dekh raha hai ...");
 
-    });
- }
+//     });
+//  }
 
- function sayHello(){
-     alert("hello mitrrrrr");
-}
+//  function sayHello(){
+//      alert("hello mitrrrrr");
+// }
 
- function sayName(){
-     alert("mara name hai namaaa");
-}
+//  function sayName(){
+//      alert("mara name hai namaaa");
+// }
+
+// let p = document.querySelector("p");
+
+// p.addEventListener("click",function(){
+//     console.log("parah was clicked");
+// });
+
+// let box = document.querySelector(".box");
+// box.addEventListener("mouseenter",function(){
+//     console.log("kaya ji shanti sa nhi rahega")
+// });
